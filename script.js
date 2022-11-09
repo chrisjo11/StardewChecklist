f1f680d5709718c61e8279b23f7c167e0a82262a
chrisjo11 <68395837+chrisjo11@users.noreply.github.com>
2022-11-09 09:55:18 -0500

    can click on things to strike them out

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-document.querySelector("p").onclick = function() {
-    let desc = document.createElement("div");
-    desc.innerHTML = '<div> <p id="commentText">asdf</p> </div>';
-}
+Array.from(document.getElementsByClassName("checkItem")).forEach(e => {
+    e.onclick = function() {
+        e.style.setProperty("text-decoration", "line-through");
+    }
+});
