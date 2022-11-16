fcae77edb7c4001c87c6606c031e515d1c439310
chrisjo11 <68395837+chrisjo11@users.noreply.github.com>
2022-11-15 23:43:04 -0500

    progress bar

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+var element = document.getElementById("progressBar");
+var width = 2.5;
+
 Array.from(document.getElementsByClassName("checkItem")).forEach(e => {
     e.onclick = function() {
         e.style.setProperty("text-decoration", "line-through");
         e.style.setProperty("color", "gray");
+        width += 2.5; 
+        element.style.width = width + '%'; 
     }
 });
