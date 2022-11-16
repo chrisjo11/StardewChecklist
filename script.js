26e74a880cdc246a612712f5d28d82b9e839cb77
chrisjo11 <68395837+chrisjo11@users.noreply.github.com>
2022-11-16 10:41:31 -0500

    failed to confetti

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
+const ConfettiGenerator = require("confetti-js");
+
 var element = document.getElementById("progressBar");
-var width = 2.5;
+var width = 0;
 let click = 1;
 let clicked = [];
 
 Array.from(document.getElementsByClassName("checkItem")).forEach(e => {
-        e.onclick = function() {
-            if(!clicked.includes(e)) {
+    e.onclick = function () {
+        if (!clicked.includes(e)) {
             e.style.setProperty("text-decoration", "line-through");
             e.style.setProperty("color", "gray");
-            width += 2.5; 
-            element.style.width = width + '%'; 
+            width += 2.5;
+            element.style.width = width + '%';
             clicked.push(e);
         }
     }
 });
 
-document.querySelector("button").onclick = function() {
-    if(click%2 != 0) {
+document.querySelector("button").onclick = function () {
+    if (click % 2 != 0) {
         console.log("hola");
         document.body.style.backgroundImage = "url(images/svdark.png)";
         document.body.style.backgroundRepeat = "no-repeat";
         document.body.style.backgroundPosition = "center";
         document.body.style.backgroundSize = "cover";
         document.body.style.backgroundAttachment = "fixed";
-    } else if(click%2 == 0) {
+    } else if (click % 2 == 0) {
         console.log("hello");
         document.body.style.backgroundImage = "url(images/svlight.png)";
         document.body.style.backgroundRepeat = "no-repeat";
