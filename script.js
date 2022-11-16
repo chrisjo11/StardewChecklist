b46681e27e61ed9c55e927b7e98d17790dbe5ef9
chrisjo11 <68395837+chrisjo11@users.noreply.github.com>
2022-11-15 23:05:06 -0500

    made site look prettier and more centered overall

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 Array.from(document.getElementsByClassName("checkItem")).forEach(e => {
     e.onclick = function() {
         e.style.setProperty("text-decoration", "line-through");
+        e.style.setProperty("color", "gray");
     }
 });
